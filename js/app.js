ed9ec8ad6375e6e57017888a20c573b597982f34
hermesalexis <hermesagm@gmail.com>
2017-02-03 14:04:51 -0500

    se genera el puntaje

--- js/app.js
+++ js/app.js
@@ -1,23 +1,38 @@
-var correct = Math.floor(Math.random() * 2); 
+var correct;
 
-$('.option').on('click',function(){
-  var index = $('.option').index(this)
-  if(index == correct){
-  	alert("Muy bien!!!!!!!!");
-  }else{
-  	alert(("No que malll"));
-  }
-});
+//inicializa el juego
+game();
 
-$('.option').each(function(index){
-  var color = generateColor();
-  $(this).css('background-color', color);
-  if(index == correct){
-  	$('.question').text(color);
-  }
-});
+//enlaza el click de los circulos a la funcion guess
+$('.option').on('click',guess);
 
 
+function game(){
+	correct = Math.floor(Math.random() * 2); 
+	
+	$('.option').each(function(index){
+	  var color = generateColor();
+	  $(this).css('background-color', color);
+	  if(index == correct){
+	  	$('.question').text(color);
+	  }
+	});
+}
+
+function guess(){
+    var index = $('.option').index(this)
+    if(index == correct){
+  	  alert("Muy bien!!!!!!!!");
+  	 var score =  $('.score').find('span').text();
+  	 $('.score').find('span').text(+score + 1);
+
+    }else{
+  	  alert(("No que malll"));
+  	  $('.score').find('span').text(0);
+
+   }
+   game();
+}
 
 
 
